03a33e20a906aba1324e1ca8dc9a6e5f4d871ceb
Ibrahim Abedghane <abedghaneibrahim94@gmail.com>
2024-05-30 20:47:39 +0000

    added utility function for creating grids

--- run.py
+++ run.py
@@ -1,2 +1,11 @@
-import ranom 
+import random 
 
+def create_grid(size):
+    """Creates a size x size grid initialized with '~'."""
+    return [['~' for _ in range(size)] for _ in range(size)]
+
+def print_grid(grid):
+    """Prints the grid."""
+    for row in grid:
+        print(' '.join(row))
+    print()
